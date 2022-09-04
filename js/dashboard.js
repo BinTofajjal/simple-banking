6ffcc13dff570edb8f7d34330e1dca36be1d932f
BinTofajjal <HabibBinTofajjal@icloud.com>
2022-09-04 14:21:04 +0600

    Deposit Input Value and Deposit Amount Synced

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,19 @@
 // Deposit Event Handler
 document.getElementById('deposit-btn').addEventListener('click', function () {
-    // Get Deposit value
-    const deposit = document.getElementById('user-deposit');
-    const depositText = deposit.value;
-    const depositAmount = parseFloat(depositText);
-    console.log(depositAmount);
+    // Get Deposit value.
+    const depositInput = document.getElementById('user-deposit');
+    const depositInputText = depositInput.value;
+    // Converting input value as Number.
+    const depositInputAmount = parseFloat(depositInputText);
+    // Add Deposit value to Deposit Amount.
+    const depositAmount = document.getElementById('deposit-amount');
+    const depositValue = depositAmount.innerText;
+    // Converting Existing value as Number.
+    const depositValueAmount = parseFloat(depositValue);
+    // Adding two values
+    const newDeposit = depositValueAmount + depositInputAmount;
+    // Setting up Deposit input Value to Deposit Amount.
+    depositAmount.innerText = newDeposit;
+    // Clear input value afer Deposit.
+    depositInput.value = '';
 })
