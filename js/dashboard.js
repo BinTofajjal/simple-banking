1a1aeeddb244c1bee7e17e3812e97aa2a3ffb385
BinTofajjal <HabibBinTofajjal@icloud.com>
2022-09-04 18:12:06 +0600

    Deposit and Balance Sycned

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,11 @@
 // Deposit Event Handler
 document.getElementById('deposit-btn').addEventListener('click', function () {
-    // Get Deposit value.
+    // Getting Deposit value.
     const depositInput = document.getElementById('user-deposit');
     const depositInputText = depositInput.value;
     // Converting input value as Number.
     const depositInputAmount = parseFloat(depositInputText);
-    // Add Deposit value to Deposit Amount.
+    // Adding Deposit value to Deposit Amount.
     const depositAmount = document.getElementById('deposit-amount');
     const depositValue = depositAmount.innerText;
     // Converting Existing value as Number.
@@ -14,6 +14,16 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const newDeposit = depositValueAmount + depositInputAmount;
     // Setting up Deposit input Value to Deposit Amount.
     depositAmount.innerText = newDeposit;
+    // Balance adjustment afer Deposit.
+    const balance = document.getElementById('balance-amount');
+    // Getting Current Balance and it's Value.
+    const balanceValueText = balance.innerText;
+    const balanceValue = parseFloat(balanceValueText);
+    // Adding Deposit value with input Deposit.
+    const balanceAfDeposit = balanceValue + depositInputAmount;
+    balance.innerText = balanceAfDeposit;
+
+
     // Clear input value afer Deposit.
     depositInput.value = '';
 })
